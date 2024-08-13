a348f2a666c445c917a7026446551f199af5b3a2
Denilson Uariua <denuariua@gmail.com>
2024-08-13 15:26:05 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+
+        MsgBox("Hello World")
+        MsgBox("How are")
+        MsgBox("I Hope all is well")
+        MsgBox("Bye!!!")
+
+
+
+    End Sub
+End Class
